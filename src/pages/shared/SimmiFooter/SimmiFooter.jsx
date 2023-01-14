9fc0c192f8877f06d00d8c4abf12f73e71d0a888
Rubel Hossain <rubrex96@hotmail.com>
2023-01-14 16:03:14 +0600

    Header and Footer component added to shared folder

---
+++ src/pages/shared/SimmiFooter/SimmiFooter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SimmiFooter = () => {
+  return <div>SimmiFooter</div>;
+};
+
+export default SimmiFooter;
---
+++ src/pages/shared/SimmiHeader/SimmiHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SimmiHeader = () => {
+  return <div>Header</div>;
+};
+
+export default SimmiHeader;
